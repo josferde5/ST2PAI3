9a43b4b83bce7e75869394831e4b0adb1a5b6ddc
José Ramón Fernández <josferde5@alum.us.es>
2021-04-22 13:24:30 +0200

    Validación de usuario y contraseña en servidor

--- prueba.py
+++ prueba.py
@@ -1,6 +1,5 @@
 import socket
 import ssl
-import time
 
 if __name__ == "__main__":
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
@@ -9,12 +8,17 @@ if __name__ == "__main__":
     context.load_cert_chain(keyfile="C:/client.key", certfile="C:/client.crt")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        with context.wrap_socket(s, server_hostname='localhost') as ssock:
-
-            # print(ssock.getpeercert())
+        with context.wrap_socket(s, server_hostname='example.com') as ssock:
 
             ssock.connect(("127.0.0.1", 8443))
             print("CLIENT INFO: Client up and connected to login server.")
-            ssock.sendall(bytes("prueba", 'utf-8'))
-            time.sleep(3)
+            ssock.sendall(bytes("st2pai3, prueba, Mensajito", 'utf-8'))
+            while True:
+                data = ssock.recv(2048)
+                if not data:
+                    continue
+
+                received_info = str(data, 'utf-8')
+                print("Received from server: " + received_info)
+                break
 
--- prueba2.py
+++ prueba2.py
@@ -1,5 +1,9 @@
 import socket
 import ssl
+import time
+
+username = "st2pai3"
+passwd = "prueba"
 
 if __name__ == "__main__":
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
@@ -10,14 +14,23 @@ if __name__ == "__main__":
         s.bind(('127.0.0.1', 8443))
         s.listen(1)
         with context.wrap_socket(s, server_side=True) as ssock:
-            connection, a = ssock.accept()
             print('SERVER INFO: Server up, waiting for a connection.')
-            print('SERVER INFO: received connection from', connection.getpeercert())
+            connection, a = ssock.accept()
+            print('SERVER INFO: received connection from', a)
             while True:
                 data = connection.recv(2048)
                 if not data:
                     continue
                 received_info = str(data, 'utf-8')
                 print("SERVER - Message received: " + received_info)
+                received_info_sp = received_info.split(",")
+                if len(received_info_sp) < 2:
+                    connection.sendall(bytes("Username and password required", "utf-8"))
+                if received_info_sp[0].strip() != username or received_info_sp[1].strip() != passwd:
+                    connection.sendall(bytes("Bad credentials: username or password incorrect", "utf-8"))
+                else:
+                    connection.sendall(bytes("Message received successfully", "utf-8"))
+                time.sleep(1)
                 break
+
 
